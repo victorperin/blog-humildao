80dfd7fc1e9882977cfb4a1fdd8d8bed68d43ceb
Victor Perin <me@victorperin.com>
2015-07-04 15:18:15 -0700

    Criando projeto pelo VisualStudio

---
+++ blog-humilde/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(blog_humilde.Startup))]
+namespace blog_humilde
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
