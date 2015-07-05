9616819557a2b0a692f223b8b55eba62fb4d9659
Victor Perin <me@victorperin.com>
2015-07-05 20:09:52 -0300

    Criando uma pagina inicial

---
+++ blog-humildao/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace blog_humildao.Models
+{
+    public class IndexModel
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string Comments { get; set; }
+    }
+}
