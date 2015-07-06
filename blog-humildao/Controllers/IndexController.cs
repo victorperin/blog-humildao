2e8ed0d2098ceb608d5501379d5c6bee5ca51e28
Victor Perin <me@victorperin.com>
2015-07-06 00:55:23 -0300

    Inserindo os exemplos, enquanto o MySQL não é inserido completamente.

--- blog-humildao/Controllers/IndexController.cs
+++ blog-humildao/Controllers/IndexController.cs
@@ -12,6 +12,28 @@ namespace blog_humildao.Controllers
         public ActionResult Index()
         {
             List<Models.Post> posts = new List<Models.Post>();
+
+            //exemplo, será retirado quando for inserido o MySQL
+            Models.Post post1 = new Models.Post
+            {
+                id = 1,
+                usuario = "Jubiscreuson Welingstom",
+                data = new DateTime(2015, 06, 09, 16, 5, 7),
+                titulo = "Second Post",
+                conteudo = "blablabla,sdaisdjqwe<br/>sidaojqwe"
+            };
+            Models.Post post2 = new Models.Post
+            {
+                id = 1,
+                usuario = "Jubiscreuson Welingstom",
+                data = new DateTime(2015, 06, 08, 10, 15, 23),
+                titulo = "First Post",
+                conteudo = "huehue! br? br?<br/>sidaojqwe"
+            };
+            posts.Add(post1);
+            posts.Add(post2);
+            //exemplo, será retirado quando for inserido o MySQL
+
             ViewData["posts"] = posts;
 
             return View();
