c6ecb256470a0261fc9b5673345d5980b7942ee5
Victor Perin <me@victorperin.com>
2015-07-06 00:47:36 -0300

    criando objeto post

---
+++ blog-humildao/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace blog_humildao.Models
+{
+    public class Post
+    {
+        public int id;
+        public string usuario;
+        public List<string> categorias;
+        public string titulo;
+        public DateTime data;
+        public string conteudo;
+    }
+}
