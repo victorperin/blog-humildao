41e5205eab551ebb542f1603f978a9caa657504f
Victor Perin <me@victorperin.com>
2015-07-09 03:18:13 -0300

    Criando o getAllPosts(), pois assim é possível receber todos posts de uma só vez.

--- blog-humildao/Models/IndexModel.cs
+++ blog-humildao/Models/IndexModel.cs
@@ -3,9 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace blog_humildao.Models
-{
-    public class IndexModel
-    {
+namespace blog_humildao.Models{
+    public static class IndexModel{
+        public static IList<Post> getAllPosts(){
+            IList<Post> posts = new List<Post>();
+            MySQL mysql = new MySQL();
+            string selectPosts = "select posts.id,usuarios.nome_exibicao,posts.data,posts.titulo,posts.conteudo from posts left join usuarios on posts.id_usuario = usuarios.id";
+            IList<IDictionary<string, string>> query = mysql.selectQuery(selectPosts);
+            for (int x = 0; x < query.Count; x++){
+                List<string> categoriasDoPost = new List<string>();
+                string selectCategorias = "select categorias.nome from posts_categorias left join categorias on posts_categorias.id_categoria = categorias.id where posts_categorias.id_post =" + query[x]["id"];
+                IList<IDictionary<string, string>> queryCategorias = mysql.selectQuery(selectCategorias);
+                for (int y = 0; y < queryCategorias.Count; y++){
+                    categoriasDoPost.Add(queryCategorias[y]["nome"]);
+                }
+                posts.Add(new Post
+                {
+                    id = int.Parse(query[x]["id"]),
+                    usuario = query[x]["nome_exibicao"],
+                    data = DateTime.Parse(query[x]["data"]),
+                    titulo = query[x]["titulo"],
+                    conteudo = query[x]["conteudo"],
+                    categorias = categoriasDoPost
+                });
+            
+            }
+            return posts;
+        }
     }
 }
