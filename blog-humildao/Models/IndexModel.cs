781843fe650a4944d40c735753d5cdbca275a8f2
Victor Perin <me@victorperin.com>
2015-07-09 21:35:33 -0300

    Inserindo método para receber as categorias do post

--- blog-humildao/Models/IndexModel.cs
+++ blog-humildao/Models/IndexModel.cs
@@ -5,18 +5,13 @@ using System.Web;
 
 namespace blog_humildao.Models{
     public static class IndexModel{
+        private static MySQL mysql = new MySQL();
         public static IList<Post> getAllPosts(){
             IList<Post> posts = new List<Post>();
-            MySQL mysql = new MySQL();
             string selectPosts = "select posts.id,usuarios.nome_exibicao,posts.data,posts.titulo,posts.conteudo from posts left join usuarios on posts.id_usuario = usuarios.id";
             IList<IDictionary<string, string>> query = mysql.selectQuery(selectPosts);
             for (int x = 0; x < query.Count; x++){
-                List<string> categoriasDoPost = new List<string>();
-                string selectCategorias = "select categorias.nome from posts_categorias left join categorias on posts_categorias.id_categoria = categorias.id where posts_categorias.id_post =" + query[x]["id"];
-                IList<IDictionary<string, string>> queryCategorias = mysql.selectQuery(selectCategorias);
-                for (int y = 0; y < queryCategorias.Count; y++){
-                    categoriasDoPost.Add(queryCategorias[y]["nome"]);
-                }
+                List<string> categoriasDoPost = getCategoriasPost(int.Parse(query[x]["id"]));
                 posts.Add(new Post
                 {
                     id = int.Parse(query[x]["id"]),
@@ -29,6 +24,16 @@ namespace blog_humildao.Models{
             
             }
             return posts;
+        }
+        public static List<string> getCategoriasPost(int idPost){
+            List<string> categoriasDoPost = new List<string>();
+            string selectCategorias = "select categorias.nome from posts_categorias left join categorias on posts_categorias.id_categoria = categorias.id where posts_categorias.id_post =" + idPost;
+            IList<IDictionary<string, string>> queryCategorias = mysql.selectQuery(selectCategorias);
+            for (int y = 0; y < queryCategorias.Count; y++){
+                categoriasDoPost.Add(queryCategorias[y]["nome"]);
+            }
+            return categoriasDoPost;
+                        
         }
     }
 }
