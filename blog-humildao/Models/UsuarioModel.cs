702cbaa4c57a00afe4ef9de06f2abf0209eccede
Victor Perin <me@victorperin.com>
2015-07-24 10:42:04 -0300

    Inserindo objeto Usuário.

---
+++ blog-humildao/Models/UsuarioModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace blog_humildao.Models
+{
+    public class UsuarioModel
+    {
+        public string id { get; set; }
+        public string login { get; set; }
+        public string nomeExibicao { get; set; }
+        public string email { get; set; }
+        public string admin { get; set; }
+        public string hashSenha { get; set; }
+        public string saltSenha { get; set; }
+    }
+}
