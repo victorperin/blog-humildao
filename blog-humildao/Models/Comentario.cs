888a1804e5b0cc1098d6b62799ab7dcfdd870673
Victor Perin <me@victorperin.com>
2015-07-28 13:03:45 -0300

    inserindo objeto comentário

---
+++ blog-humildao/Models/Comentario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace blog_humildao.Models
+{
+    public class Comentario
+    {
+        public int id;
+        public string nomeExibicaoUsuario;
+        public string conteudo;
+        public string status;
+    }
+}
